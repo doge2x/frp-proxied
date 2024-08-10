033613c5124da2c3fd6928d38ba1298fc2f522d0
Doge2x <doge2x@outlook.com>
2024-08-10 09:21:16 +0800

    refactor: detect connection source by domain

--- scripts/interpolate.sh
+++ scripts/interpolate.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
 SERVER_ADDR="${SERVER_ADDR:-127.0.0.1}"
-REMOTE_UUID="${REMOTE_UUID:-$(uuidgen)}"
-CLIENT_UUID="${CLIENT_UUID:-$(uuidgen)}"
+PROXY_UUID="${PROXY_UUID:-$(uuidgen)}"
 find . \( -name '*.json' -o -name '*.toml' \) -exec \
 	sed -i \
 	-e "s/{{SERVER_ADDR}}/$SERVER_ADDR/" \
-	-e "s/{{REMOTE_UUID}}/$REMOTE_UUID/" \
-	-e "s/{{CLIENT_UUID}}/$CLIENT_UUID/" \
+	-e "s/{{PROXY_UUID}}/$PROXY_UUID/" \
 	{} \;
